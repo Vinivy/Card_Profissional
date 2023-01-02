3fdba675781291f9bf35828bfc0198c9568ebf36
Vinivy <vinivygabriel8@gmail.com>
2023-01-02 14:19:09 -0300

    NLW

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // pegar tag img
+  const img = document.querySelector('#profile img')
+
+  // substituir a imagem
+  if (html.classList.contains('light')) {
+    // se tiver light mode,  adicionar a imagem light
+    img.setAttribute('src', './Assents/EU-light.png')
+  } else {
+    // se tiver sem o light mode, manter normal
+    img.setAttribute('src', './Assents/EU.png')
+  }
+}
